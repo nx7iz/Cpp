bd90b159d152b69d376955aa42c023fd511d9f74
nx7iz <nomannz500@gmail.com>
2025-03-10 21:42:14 +0500

    work on new topics

---
+++ constructors.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+// Copy constructor
+class Car {
+public:
+  string name;
+  int modelYear;
+
+  Car (string a, int y) {
+    name = a;
+    modelYear = y;
+  }
+  // Copy constructor
+  Car (Car &obj) {
+    name = obj.name;
+    modelYear = obj.modelYear;
+  }
+  void display() {
+    cout << "Brand: " << name << "\nModel: " << modelYear << endl; 
+
+  }
+};
+
+int main() {
+  Car info("Pagani", 2019);
+  Car info2 = info;
+  info.display();
+  info2.display();
+}
+
+
+// // 1. Default Constructor
+// class Car {
+// public:
+
+// };
+
+// int main() {
+//   Car name;
+//   cout << "Done" << endl;
+// }
+
+
+// // 2. Parameterized constructor
+// class Car {
+// public:
+//   string name;
+//   int modelYear;
+
+//   Car (string name, int modelYear) {
+//     this ->name = name;
+//     this ->modelYear = modelYear;
+//   }
+
+//   void display() {
+//     cout << "Brand: " << name << "\nModel: " << modelYear << endl;
+//   }
+// };
+
+// int main() {
+//   Car info("BMW", 2019);
+//   // cout << "Brand: " << info.name << "\nModel: " << info.modelYear << endl;
+//   info.display();
+// }
+
+
+// // 3. No-Argument constructor
+// class Car {
+// public:
+//   string name;
+//   int modelYear;
+
+//   Car () {
+//     cout << "Brand: " << name << "\nModel: " << modelYear << endl; 
+//   }
+
+// };
+
+// int main() {
+//   Car info;
+
+// }
