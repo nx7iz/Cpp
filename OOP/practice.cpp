f25f05092c00bc9ca8f1bbb9520f0e63bad8e590
nx7iz <nomannz500@gmail.com>
2025-03-26 17:17:59 +0500

    practice

--- OOP/practice.cpp
+++ OOP/practice.cpp
@@ -2,37 +2,27 @@
 
 using namespace std;
 
-class Student {
+class Language {
 public:
-  string name;
-  int age;
-  static int count;
-  static string universityName;
+  static string defaultLanguage;
 
-  Student (string name, int age) {
-    this->name = name;
-    this->age = age;
-    count++;
+  Language(string language) {
+    if (defaultLanguage.empty()) {
+      defaultLanguage = language;
+    }
   }
 
-  static void displayUniversityDetails() {
-    cout << "University name: " << universityName << endl
-         << "Students: " << count << endl;
+  static string getTotalBalance() {
+    return defaultLanguage;
   }
-  void details() {
-    cout << "Student: " << count << endl
-         << "Name: " << name << endl
-         << "Age: " << age <<  endl;
-  }
+
 };
 
-string Student::universityName = "Oblivion Bench III";
-int Student::count = 0;
+string Language::defaultLanguage;
 
 int main() {
-  Student student1("Arceus", 21);
-  student1.details();
-  Student student2("Liqx", 30);
-  Student::displayUniversityDetails(); 
-  student2.details();
-} 
+  Language language1("English");
+
+  cout << "\nDefault language: " << Language::defaultLanguage;
+  
+}
